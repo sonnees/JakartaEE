731c9f6c8e57b2b3543872e024ac0ad2b4f63a95
sonnees <sson12131415@gmail.com>
2023-10-24 15:50:31 +0700

    init

--- src/main/java/vn/edu/iuh/fit/Application.java
+++ src/main/java/vn/edu/iuh/fit/Application.java
@@ -2,13 +2,12 @@ package vn.edu.iuh.fit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ApplicationConfig {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationConfig.class, args);
+        SpringApplication.run(Application.class, args);
     }
+
 }
--- src/main/java/vn/edu/iuh/fit/BackEnd/repositories/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.iuh.fit.BackEnd.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import vn.edu.iuh.fit.BackEnd.entity.Candidate;
-
-@RepositoryRestResource(path = "Candidate")
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-}
